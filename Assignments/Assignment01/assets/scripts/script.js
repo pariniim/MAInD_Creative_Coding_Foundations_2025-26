928c351e8957c389fc29f78191adbb90164ae16a
Isabel M. Parini <pariniim@gmail.com>
2025-10-30 14:22:16 +0100

    Update code

--- Assignments/Assignment01/assets/scripts/script.js
+++ Assignments/Assignment01/assets/scripts/script.js
@@ -1,41 +1,49 @@
 /*Code for section A*/
-const CHANGETOSALMONBTN = document.getElementById("light-salmon");
-const CHANGETOTURQUOISEBTN = document.getElementById("turquoise");
-const CHANGETOVIOLETBTN = document.getElementById("violet");
-const CHANGETOGREYBTN = document.getElementById("grey");
+const CHANGE_TO_SALMON_BTN = document.getElementById("light-salmon");
+const CHANGE_TO_TURQUOISE_BTN = document.getElementById("turquoise");
+const CHANGE_TO_VIOLET_BTN = document.getElementById("violet");
+const CHANGE_TO_GREY_BTN = document.getElementById("grey");
 
-const FIRSTSECTION = document.getElementById("section-one");
+const FIRST_SECTION = document.getElementById("section-one");
 
-CHANGETOSALMONBTN.addEventListener("click", () => {
-    FIRSTSECTION.style.backgroundColor = "lightsalmon";
+CHANGE_TO_SALMON_BTN.addEventListener("click", () => {
+    FIRST_SECTION.style.backgroundColor = "lightsalmon";
 })
 
-CHANGETOTURQUOISEBTN.addEventListener("click", () => {
-    FIRSTSECTION.style.backgroundColor = "turquoise";
+CHANGE_TO_TURQUOISE_BTN.addEventListener("click", () => {
+    FIRST_SECTION.style.backgroundColor = "turquoise";
 })
 
-CHANGETOVIOLETBTN.addEventListener("click", () => {
-    FIRSTSECTION.style.backgroundColor = "violet";
+CHANGE_TO_VIOLET_BTN.addEventListener("click", () => {
+    FIRST_SECTION.style.backgroundColor = "violet";
 })
 
-CHANGETOGREYBTN.addEventListener("click", () => {
-    FIRSTSECTION.style.backgroundColor = "grey";
+CHANGE_TO_GREY_BTN.addEventListener("click", () => {
+    FIRST_SECTION.style.backgroundColor = "grey";
 })
 
 /*Code for section B*/
-const CHANGETOVERTALIGNBTN = document.getElementById("change-to-horizontal-btn");
+const CHANGE_TO_VERT_ALIGN_BTN = document.getElementById("change-to-vertical-btn");
+const CHANGE_TO_HORIZ_ALIGN_BTN = document.getElementById("change-to-horizontal-btn");
 
-const SECONDSECTIONCHANGE = document.getElementsByClassName("element-area");
+const SECOND_SECTION_CHANGE = document.getElementById("alignment-change");
 
-CHANGETOVERTALIGNBTN.addEventListener("click", () => {
-    SECONDSECTIONCHANGE.style.flexDirection = "column";
+CHANGE_TO_VERT_ALIGN_BTN.addEventListener("click", () => {
+    SECOND_SECTION_CHANGE.style.flexDirection = "column";
+    SECOND_SECTION_CHANGE.style.justifyContent = "flex-start";
 })    
 
+CHANGE_TO_HORIZ_ALIGN_BTN.addEventListener("click", () => {
+    SECOND_SECTION_CHANGE.style.flexDirection = "row";
+    SECOND_SECTION_CHANGE.style.justifyContent = "center";
+})    
+
+
 /*Code for section C*/
-const REMOVELASTELEMENT = document.getElementById("remove-element-btn");
+const REMOVE_LAST_ELEMENT = document.getElementById("remove-element-btn");
 
-const ELEMENTFIVE = document.getElementById("last-element");
+const ELEMENT_FIVE = document.getElementById("last-element");
 
-REMOVELASTELEMENT.addEventListener("click", () => {
-    ELEMENTFIVE.remove();
+REMOVE_LAST_ELEMENT.addEventListener("click", () => {
+    ELEMENT_FIVE.remove();
 })
